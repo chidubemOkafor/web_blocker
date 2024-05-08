c43b752f4b16ad41ccea2db3223d590e9c84de88
Robinhood <okaforchidubem7@gmail.com>
2024-05-08 13:30:05 -0700

    can now block messge

--- background.js
+++ background.js
@@ -21,22 +21,21 @@
 // //   }
 // // };
 
-// const getData = (url) => {
-//   chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-//     console.log(data);
-//     if (data.length != 0 && data) findMatchingUrl(url, data);
-//   });
-// };
-// // Listen for tab updates
-// chrome.tabs.onUpdated.addListener((changeInfo) => {
-//   console.log(changeInfo);
-//   if (changeInfo.status === "complete") {
-//     const currentUrl = changeInfo.url;
-//     console.log(currentUrl);
-//     console.log("this is the changed info url", currentUrl);
-//     getData(currentUrl);
-//   }
-// });
+const getData = (url) => {
+  chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+    console.log(data);
+  });
+};
+// Listen for tab updates
+chrome.tabs.onUpdated.addListener((changeInfo) => {
+  console.log(changeInfo);
+  if (changeInfo.status === "complete") {
+    // const currentUrl = changeInfo.url;
+    // console.log(currentUrl);
+    // console.log("this is the changed info url", currentUrl);
+    // getData(currentUrl);
+  }
+});
 
 // chrome.tabs.onActivated.addListener((activeInfo) => {
 //   // how to fetch tab url using activeInfo.tabid
--- content_script.js
+++ content_script.js
@@ -1,5 +1,26 @@
 console.log("this is the content script.js");
 
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-  console.log("Message received in content script:", data);
+  blockSite(data);
 });
+const blockSite = (data) => {
+  console.log("Message received in content script:", data);
+
+  const href = window.location.href;
+  console.log(href);
+
+  for (let i = 0; i < data.length; i++) {
+    if (href.includes(data[i].url)) {
+      console.log("the data is here");
+      document.body.innerHTML = `
+              <div class= "mainDIv">
+                <p class="text">
+                  The website ${href} successfully blocked !
+                </p>
+              </div>
+        `;
+      break;
+    }
+    console.log("the data is not here");
+  }
+};
